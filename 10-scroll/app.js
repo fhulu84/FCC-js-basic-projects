37df748f18c8b9d8b6cabd61dba547ef0974f25e
fhulu84 <fhulu84@users.noreply.github.com>
2021-01-04 23:53:38 +0000

    scroll - date&close links

--- 10-scroll/app.js
+++ 10-scroll/app.js
@@ -4,9 +4,35 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
+const date = document.getElementById('date');
+date.innerHTML = new Date().getFullYear();
 
 // ********** close links ************
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
 
+navToggle.addEventListener('click', function(){
+  // this might be a comcern if the links count changes, not enough size or more than needed, since height is set static
+  // linksContainer.classList.toggle('show-links');
+
+  // set the height dynamically
+  // with links-container wrapper div we can hide the links initially by setting its height to 0, but links height is still available to get the size of links area 
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+  
+  if(containerHeight === 0){ //means default setup, we gonna show the links
+    linksContainer.style.height = `${linksHeight}px`;
+  } else { // we gonna hide the links
+    linksContainer.style.height = 0;
+  }
+
+  // here we are changing inline style so it has the highest priority, so when screen size changed we need to set the styling for links-container's height as auto with !important keyword
+  /// .links-container {
+  //   height: auto !important;
+  // }
+  
+})
 // ********** fixed navbar ************
 
 // ********** smooth scroll ************
