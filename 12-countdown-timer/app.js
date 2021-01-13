8298514fe62a184f259f29fa1d539e289346cd15
fhulu84 <fhulu84@users.noreply.github.com>
2021-01-13 23:53:00 +0000

    index.html

---
+++ 12-countdown-timer/app.js
@@ -0,0 +1,39 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const deadline = document.querySelector('.deadline');
+const giveaway = document.querySelector('.giveaway');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let futureDate = new Date(2021,0,24,11,30,0);
+console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const mins = futureDate.getMinutes();
+
+let month = months[futureDate.getMonth()];
+console.log(month);
+
+giveaway.textContent = `giveaway ends on ${year} ${hours}:${mins}am`;
